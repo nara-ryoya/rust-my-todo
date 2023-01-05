b2fef27eb9a6f3ccb674a1fc206b921b8008cab7
nara-ryoya <nara_ryoya@icloud.com>
2023-01-06 06:16:20 +0900

    feat: logging

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use axum::{routing::get, Router};
+use std::env;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing_subscriber::fmt::init();
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    tracing::debug!("listening on {}", addr);
 }
 
 async fn root() -> &'static str {
